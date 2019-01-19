05650df7919b8448896a5d75482e856b84a0bb93
Swaroop Sridhar <Swaroop.Sridhar@microsoft.com>
2019-01-18 18:26:10 -0800

    C# bundle tool

---
+++ src/tools/bundle/bundle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace Microsoft.Dotnet
+{
+    class Bundle
+    {
+        static void Main(string[] args)
+        {
+            string hostName = args[0];
+            BinaryWriter host = new BinaryWriter(File.Open(hostName, FileMode.Open));
+
+            string[] files = Directory.GetFiles(args[1]);
+            foreach (string file in files)
+            {
+                if(Path.GetFullPath(file).Equals(Path.GetFullPath(hostName)))
+                {
+                    Console.WriteLine("Skip Host");
+                    continue;
+                }
+
+                BinaryReader resource = new BinaryReader(File.Open(file, FileMode.Open));
+
+
+
+            }
+
+            GetFiles(String, String)
+
+            using (BinaryReader reader = new BinaryReader(File.Open(fileName, FileMode.Open)))
+            {
+
+            }
+
+        }
+    }
+}
