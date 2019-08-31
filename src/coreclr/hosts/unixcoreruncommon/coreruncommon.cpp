d459763aab3d437dbe6ee8a1540be9484a64f866
Swaroop Sridhar <Swaroop.Sridhar@microsoft.com>
2019-08-30 18:17:55 -0700

    Fix AppPath 2

--- src/coreclr/hosts/unixcoreruncommon/coreruncommon.cpp
+++ src/coreclr/hosts/unixcoreruncommon/coreruncommon.cpp
@@ -327,7 +327,6 @@ int ExecuteManagedAssembly(
 
     // Get just the path component of the managed assembly path
     std::string appPath;
-    //GetDirectory(currentExeAbsolutePath, appPath);
 
     std::string tpaList;
     if (strlen(managedAssemblyAbsolutePath) > 0)
